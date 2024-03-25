2b073e779edab6e69c69dcbe0ab2333afb348476
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-03-25 15:46:30 +0200

    Configured a range of hyper-params

--- configs/models_config_for_tuning.py
+++ configs/models_config_for_tuning.py
@@ -1,18 +1,19 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neural_network import MLPClassifier
+from pprint import pprint
 from lightgbm import LGBMClassifier
 
 
 def get_models_params_for_tuning(models_tuning_seed):
     return {
-        'LGBMClassifier': {
-            'model': LGBMClassifier(random_state=models_tuning_seed),
+        'DecisionTreeClassifier': {
+            'model': DecisionTreeClassifier(random_state=models_tuning_seed),
             'params': {
-                'max_depth' : [i for i in range(3,12)],
-                'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
-                'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+                "max_depth": [5, 10, 20, 30],
+                'min_samples_leaf': [5, 10, 20, 50, 100],
+                "max_features": [0.6, 'sqrt'],
+                "criterion": ["gini", "entropy"]
             }
         },
         'LogisticRegression': {
@@ -23,12 +24,17 @@ def get_models_params_for_tuning(models_tuning_seed):
                 'solver': ['newton-cg', 'lbfgs', 'sag', 'saga'],
             }
         },
-        'MLPClassifier': {
-            'model': MLPClassifier(hidden_layer_sizes=(100,100,), random_state=models_tuning_seed, max_iter=1000),
+        'LGBMClassifier': {
+            'model': LGBMClassifier(random_state=models_tuning_seed),
             'params': {
-                'activation': ['logistic', 'tanh', 'relu'],
-                'solver': ['lbfgs', 'sgd', 'adam'],
-                'learning_rate': ['constant', 'invscaling', 'adaptive']
+                'max_depth' : [i for i in range(3, 12)],
+                'num_leaves' : [int(x) for x in np.linspace(start = 20, stop = 3000, num = 10)],
+                'min_data_in_leaf' : [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)],
             }
-        }
+        },
     }
+
+
+if __name__ == '__main__':
+    model_tuning_params = get_models_params_for_tuning(models_tuning_seed=200)
+    pprint(model_tuning_params)
