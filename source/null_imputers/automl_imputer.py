078ff38f34e6821d5be5f47f280f3a442e1c5d44
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-04-09 00:55:43 +0300

    Modified automl imputer

--- source/null_imputers/automl_imputer.py
+++ source/null_imputers/automl_imputer.py
@@ -240,7 +240,9 @@ class AutoMLImputer(BaseImputer):
         # =============================================================================================================
         # 2) Create a list of column names sorted by the number of nulls in them
         # =============================================================================================================
+        print('X.isnull().sum():\n', X.isnull().sum())
         sorted_columns_names_by_nulls = get_columns_sorted_by_nulls(X[self._target_columns])
+        print('sorted_columns_names_by_nulls -- ', sorted_columns_names_by_nulls)
 
         # =============================================================================================================
         # 3) Fit a predictor for each column with nulls. Start from the column with the smallest portion of nulls.
