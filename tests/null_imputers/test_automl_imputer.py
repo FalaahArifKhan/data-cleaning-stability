3ca6aad51898a4f03036002b9c4ee75cc77ea0fc
denysgerasymuk799 <herasymuk@ucu.edu.ua>
2024-04-13 19:54:41 +0300

    Added tests for automl

--- tests/null_imputers/test_automl_imputer.py
+++ tests/null_imputers/test_automl_imputer.py
@@ -60,115 +60,103 @@ def test_automl_imputer_no_nulls(acs_income_dataset_params, null_imputer_name,
     assert not X_test_imputed.isnull().any().any(), "X_test_imputed contains null values"
 
 
-# # Test if automl returns same results with the same seed
-# def test_automl_imputer_same_seed(acs_income_dataset_params, null_imputer_name,
-#                                   mcar_mar_evaluation_scenario, common_seed, automl_kwargs):
-#     # Init function variables
-#     dataset_name = ACS_INCOME_DATASET
-#     evaluation_scenario = mcar_mar_evaluation_scenario
-#     experiment_seed = common_seed
-#     imputation_kwargs = automl_kwargs
-#
-#     train_injection_strategy, _ = parse_evaluation_scenario(evaluation_scenario)
-#     hyperparams = NULL_IMPUTERS_HYPERPARAMS.get(null_imputer_name, {}).get(dataset_name, {}).get(train_injection_strategy, {})
-#
-#     (X_train_with_nulls, X_test_with_nulls,
-#      train_numerical_null_columns, train_categorical_null_columns,
-#      numerical_columns, categorical_columns) = acs_income_dataset_params
-#
-#     # Apply automl
-#     output_path = (pathlib.Path(__file__).parent.parent.parent
-#                    .joinpath('results')
-#                    .joinpath('tests')
-#                    .joinpath(null_imputer_name)
-#                    .joinpath(dataset_name)
-#                    .joinpath(evaluation_scenario)
-#                    .joinpath(str(experiment_seed)))
-#     imputation_kwargs.update({'directory': output_path})
-#
-#     imputation_kwargs.update({'experiment_seed': experiment_seed})
-#     X_train_imputed1, X_test_imputed1, null_imputer_params_dct1 = (
-#         datawig_imputer.complete(X_train_with_nulls=X_train_with_nulls,
-#                                  X_test_with_nulls=X_test_with_nulls,
-#                                  numeric_columns_with_nulls=train_numerical_null_columns,
-#                                  categorical_columns_with_nulls=train_categorical_null_columns,
-#                                  all_numeric_columns=numerical_columns,
-#                                  all_categorical_columns=categorical_columns,
-#                                  hyperparams=hyperparams,
-#                                  output_path=output_path,
-#                                  **imputation_kwargs)
-#     )
-#     X_train_imputed2, X_test_imputed2, null_imputer_params_dct2 = (
-#         datawig_imputer.complete(X_train_with_nulls=X_train_with_nulls,
-#                                  X_test_with_nulls=X_test_with_nulls,
-#                                  numeric_columns_with_nulls=train_numerical_null_columns,
-#                                  categorical_columns_with_nulls=train_categorical_null_columns,
-#                                  all_numeric_columns=numerical_columns,
-#                                  all_categorical_columns=categorical_columns,
-#                                  hyperparams=hyperparams,
-#                                  output_path=output_path,
-#                                  **imputation_kwargs)
-#     )
-#
-#     # Check if the results are identical
-#     assert X_train_imputed1.equals(X_train_imputed2), "X_train_imputed from automl are not identical"
-#     assert X_test_imputed1.equals(X_test_imputed2), "X_test_imputed from automl are not identical"
-#     assert_nested_dicts_equal(null_imputer_params_dct1, null_imputer_params_dct2,
-#                               assert_msg="null_imputer_params_dct from automl are not identical")
-#
-#
-# # Test if automl returns different results for different seeds
-# def test_automl_imputer_diff_seed(acs_income_dataset_params, null_imputer_name,
-#                                   mcar_mar_evaluation_scenario, common_seed, automl_kwargs):
-#     # Init function variables
-#     dataset_name = ACS_INCOME_DATASET
-#     evaluation_scenario = mcar_mar_evaluation_scenario
-#     experiment_seed = common_seed
-#     imputation_kwargs = automl_kwargs
-#
-#     train_injection_strategy, _ = parse_evaluation_scenario(evaluation_scenario)
-#     hyperparams = NULL_IMPUTERS_HYPERPARAMS.get(null_imputer_name, {}).get(dataset_name, {}).get(train_injection_strategy, {})
-#
-#     (X_train_with_nulls, X_test_with_nulls,
-#      train_numerical_null_columns, train_categorical_null_columns,
-#      numerical_columns, categorical_columns) = acs_income_dataset_params
-#
-#     # Apply automl
-#     output_path = (pathlib.Path(__file__).parent.parent.parent
-#                    .joinpath('results')
-#                    .joinpath('tests')
-#                    .joinpath(null_imputer_name)
-#                    .joinpath(dataset_name)
-#                    .joinpath(evaluation_scenario)
-#                    .joinpath(str(experiment_seed)))
-#     imputation_kwargs.update({'directory': output_path})
-#
-#     imputation_kwargs.update({'experiment_seed': 100})
-#     X_train_imputed1, X_test_imputed1, null_imputer_params_dct1 = (
-#         datawig_imputer.complete(X_train_with_nulls=X_train_with_nulls,
-#                                  X_test_with_nulls=X_test_with_nulls,
-#                                  numeric_columns_with_nulls=train_numerical_null_columns,
-#                                  categorical_columns_with_nulls=train_categorical_null_columns,
-#                                  all_numeric_columns=numerical_columns,
-#                                  all_categorical_columns=categorical_columns,
-#                                  hyperparams=hyperparams,
-#                                  output_path=output_path,
-#                                  **imputation_kwargs)
-#     )
-#
-#     imputation_kwargs.update({'experiment_seed': 200})
-#     X_train_imputed2, X_test_imputed2, null_imputer_params_dct2 = (
-#         datawig_imputer.complete(X_train_with_nulls=X_train_with_nulls,
-#                                  X_test_with_nulls=X_test_with_nulls,
-#                                  numeric_columns_with_nulls=train_numerical_null_columns,
-#                                  categorical_columns_with_nulls=train_categorical_null_columns,
-#                                  all_numeric_columns=numerical_columns,
-#                                  all_categorical_columns=categorical_columns,
-#                                  hyperparams=hyperparams,
-#                                  output_path=output_path,
-#                                  **imputation_kwargs)
-#     )
-#
-#     # Check if the results are identical
-#     assert not X_train_imputed1.equals(X_train_imputed2), "X_train_imputed from automl is the same for different seeds"
-#     assert not X_test_imputed1.equals(X_test_imputed2), "X_test_imputed from automl is the same for different seeds"
+# Test if automl returns same results with the same seed
+def test_automl_imputer_same_seed(acs_income_dataset_params, null_imputer_name,
+                                  mcar_mar_evaluation_scenario, common_seed, automl_kwargs):
+    # Init function variables
+    dataset_name = ACS_INCOME_DATASET
+    evaluation_scenario = mcar_mar_evaluation_scenario
+    experiment_seed = common_seed
+    imputation_kwargs = automl_kwargs
+
+    train_injection_strategy, _ = parse_evaluation_scenario(evaluation_scenario)
+    hyperparams = NULL_IMPUTERS_HYPERPARAMS.get(null_imputer_name, {}).get(dataset_name, {}).get(train_injection_strategy, {})
+
+    (X_train_with_nulls, X_test_with_nulls,
+     train_numerical_null_columns, train_categorical_null_columns,
+     numerical_columns, categorical_columns) = acs_income_dataset_params
+
+    # Apply automl
+    output_path = (pathlib.Path(__file__).parent.parent.parent
+                   .joinpath('results')
+                   .joinpath('tests')
+                   .joinpath(null_imputer_name)
+                   .joinpath(dataset_name)
+                   .joinpath(evaluation_scenario)
+                   .joinpath(str(experiment_seed)))
+    imputation_kwargs.update({'directory': output_path})
+
+    imputation_kwargs.update({'experiment_seed': experiment_seed})
+    X_train_imputed1, X_test_imputed1, null_imputer_params_dct1 = (
+        impute_with_automl(X_train_with_nulls=X_train_with_nulls,
+                           X_test_with_nulls=X_test_with_nulls,
+                           numeric_columns_with_nulls=train_numerical_null_columns,
+                           categorical_columns_with_nulls=train_categorical_null_columns,
+                           hyperparams=hyperparams,
+                           **imputation_kwargs)
+    )
+    X_train_imputed2, X_test_imputed2, null_imputer_params_dct2 = (
+        impute_with_automl(X_train_with_nulls=X_train_with_nulls,
+                           X_test_with_nulls=X_test_with_nulls,
+                           numeric_columns_with_nulls=train_numerical_null_columns,
+                           categorical_columns_with_nulls=train_categorical_null_columns,
+                           hyperparams=hyperparams,
+                           **imputation_kwargs)
+    )
+
+    # Check if the results are identical
+    assert X_train_imputed1.equals(X_train_imputed2), "X_train_imputed from automl are not identical"
+    assert X_test_imputed1.equals(X_test_imputed2), "X_test_imputed from automl are not identical"
+    assert_nested_dicts_equal(null_imputer_params_dct1, null_imputer_params_dct2,
+                              assert_msg="null_imputer_params_dct from automl are not identical")
+
+
+# Test if automl returns different results for different seeds
+def test_automl_imputer_diff_seed(acs_income_dataset_params, null_imputer_name,
+                                  mcar_mar_evaluation_scenario, common_seed, automl_kwargs):
+    # Init function variables
+    dataset_name = ACS_INCOME_DATASET
+    evaluation_scenario = mcar_mar_evaluation_scenario
+    experiment_seed = common_seed
+    imputation_kwargs = automl_kwargs
+
+    train_injection_strategy, _ = parse_evaluation_scenario(evaluation_scenario)
+    hyperparams = NULL_IMPUTERS_HYPERPARAMS.get(null_imputer_name, {}).get(dataset_name, {}).get(train_injection_strategy, {})
+
+    (X_train_with_nulls, X_test_with_nulls,
+     train_numerical_null_columns, train_categorical_null_columns,
+     numerical_columns, categorical_columns) = acs_income_dataset_params
+
+    # Apply automl
+    output_path = (pathlib.Path(__file__).parent.parent.parent
+                   .joinpath('results')
+                   .joinpath('tests')
+                   .joinpath(null_imputer_name)
+                   .joinpath(dataset_name)
+                   .joinpath(evaluation_scenario)
+                   .joinpath(str(experiment_seed)))
+    imputation_kwargs.update({'directory': output_path})
+
+    imputation_kwargs.update({'experiment_seed': 100})
+    X_train_imputed1, X_test_imputed1, null_imputer_params_dct1 = (
+        impute_with_automl(X_train_with_nulls=X_train_with_nulls,
+                           X_test_with_nulls=X_test_with_nulls,
+                           numeric_columns_with_nulls=train_numerical_null_columns,
+                           categorical_columns_with_nulls=train_categorical_null_columns,
+                           hyperparams=hyperparams,
+                           **imputation_kwargs)
+    )
+
+    imputation_kwargs.update({'experiment_seed': 200})
+    X_train_imputed2, X_test_imputed2, null_imputer_params_dct2 = (
+        impute_with_automl(X_train_with_nulls=X_train_with_nulls,
+                           X_test_with_nulls=X_test_with_nulls,
+                           numeric_columns_with_nulls=train_numerical_null_columns,
+                           categorical_columns_with_nulls=train_categorical_null_columns,
+                           hyperparams=hyperparams,
+                           **imputation_kwargs)
+    )
+
+    # Check if the results are identical
+    assert not X_train_imputed1.equals(X_train_imputed2), "X_train_imputed from automl is the same for different seeds"
+    assert not X_test_imputed1.equals(X_test_imputed2), "X_test_imputed from automl is the same for different seeds"
