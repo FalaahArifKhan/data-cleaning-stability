2f53c9f290846e046714cffeef944e1833dd0b5c
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-04-15 22:54:13 +0300

    Added CPClean to the pipeline

--- configs/datasets_config.py
+++ configs/datasets_config.py
@@ -40,7 +40,7 @@ DATASET_CONFIG = {
     ACS_INCOME_DATASET: {
         "data_loader": ACSIncomeDataset,
         "data_loader_kwargs": {"state": ['GA'], "year": 2018, "with_nulls": False,
-                               "subsample_size": 1_000, "subsample_seed": 42},
+                               "subsample_size": 500, "subsample_seed": 42},
         "test_set_fraction": 0.2,
         "virny_config_path": pathlib.Path(__file__).parent.joinpath('yaml_files', 'acs_income_GA_2018_config.yaml')
     },
