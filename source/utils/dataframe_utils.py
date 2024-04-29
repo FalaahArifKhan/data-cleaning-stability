2f763e7b19aca49a8a82bf5ea84732c716c7a3d8
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-04-29 21:15:07 +0300

    Finalized KL divergence computation

--- source/utils/dataframe_utils.py
+++ source/utils/dataframe_utils.py
@@ -1,6 +1,8 @@
+import scipy
 import numpy as np
 import pandas as pd
 from scipy.stats import entropy, gaussian_kde
+from sklearn.preprocessing import StandardScaler
 
 from source.preprocessing import get_simple_preprocessor
 
@@ -91,31 +93,9 @@ def get_columns_sorted_by_nulls(mask):
     return sorted_columns_names
 
 
-def calculate_kl_divergence(true: pd.DataFrame, pred: pd.DataFrame, column_type: str):
-    # ========================================================
-    # Compute KL divergence for continuous numerical features
-    # ========================================================
-    if column_type == 'numerical':
-        real_n_unique = true.nunique()
-        int_n_unique = true.astype(int).nunique()
+def calculate_kl_divergence_with_histograms(true: pd.DataFrame, pred: pd.DataFrame):
+    print('Compute KL divergence using histograms...')
 
-        if real_n_unique != int_n_unique:
-            # Estimate probability density functions using kernel density estimation
-            true_kde = gaussian_kde(true)
-            pred_kde = gaussian_kde(pred)
-
-            # Evaluate KDEs at a set of points
-            x = np.linspace(min(min(true), min(pred)), max(max(true), max(pred)), 1000)
-            true_dist = true_kde.evaluate(x)
-            pred_dist = pred_kde.evaluate(x)
-
-            # Calculate KL divergence from true_dist to pred_dist
-            # KL(P || Q) where P is the true distribution and Q is the approximation
-            return entropy(true_dist, pred_dist)
-
-    # ======================================================================
-    # Compute KL divergence for categorical and discrete numerical features
-    # ======================================================================
     # Get the value counts normalized to probability distributions
     true_dist = true.value_counts(normalize=True)
     pred_dist = pred.value_counts(normalize=True)
@@ -129,4 +109,61 @@ def calculate_kl_divergence(true: pd.DataFrame, pred: pd.DataFrame, column_type:
     # KL(P || Q) where P is the true distribution and Q is the approximation
     kl_div = entropy(true_dist, pred_dist)
 
+    return kl_div
+
+
+def calculate_kl_divergence_with_kde(true: pd.DataFrame, pred: pd.DataFrame):
+    print('Compute KL divergence using KDE...')
+
+    # Normalize true and pred series
+    scaler = StandardScaler().set_output(transform="pandas")
+    true_scaled = scaler.fit_transform(true.to_frame())
+    true_scaled = true_scaled[true_scaled.columns[0]]
+    pred_scaled = scaler.fit_transform(pred.to_frame())
+    pred_scaled = pred_scaled[pred_scaled.columns[0]]
+
+    if pred.nunique() == 1:
+        # Estimate probability density functions using kernel density estimation
+        true_kde = gaussian_kde(true_scaled)
+
+        # Create the discrete uniform distribution with one value
+        discrete_uniform_values = [pred_scaled.values[0]]
+        discrete_uniform_pmf = [1.0]  # Probability mass function for the single value
+        pred_kde = scipy.stats.rv_discrete(values=(discrete_uniform_values, discrete_uniform_pmf))
+
+        # Compute the PMF/PDF for both distributions
+        x = np.linspace(min(min(true_scaled), min(pred_scaled)), max(max(true_scaled), max(pred_scaled)), 999)
+        x = np.append(x, discrete_uniform_values)
+
+        true_dist = true_kde.evaluate(x)
+        pred_dist = pred_kde.pmf(x)
+        pred_dist[pred_dist == 0.] = 0.000000001  # replace zeros to avoid NaNs in scipy.entropy
+
+    else:
+        # Estimate probability density functions using kernel density estimation
+        true_kde = gaussian_kde(true_scaled)
+        pred_kde = gaussian_kde(pred_scaled)
+
+        # Evaluate KDEs at a set of points
+        x = np.linspace(min(min(true_scaled), min(pred_scaled)), max(max(true_scaled), max(pred_scaled)), 1000)
+        true_dist = true_kde.evaluate(x)
+        pred_dist = pred_kde.evaluate(x)
+
+    # Calculate KL divergence from true_dist to pred_dist
+    # KL(P || Q) where P is the true distribution and Q is the approximation
+    return entropy(true_dist, pred_dist)
+
+
+def calculate_kl_divergence(true: pd.DataFrame, pred: pd.DataFrame, column_type: str):
+    # Compute KL divergence for continuous numerical features
+    if column_type == 'numerical':
+        real_n_unique = true.nunique()
+        int_n_unique = true.astype(int).nunique()
+
+        if real_n_unique != int_n_unique:
+            kl_div = calculate_kl_divergence_with_kde(true, pred)
+            return kl_div
+
+    # Compute KL divergence for categorical and discrete numerical features
+    kl_div = calculate_kl_divergence_with_histograms(true, pred)
     return kl_div
