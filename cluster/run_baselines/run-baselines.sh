0517bb5c0f287235486bfdebcb3601ea219c70fa
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-05-02 02:50:42 +0300

    Prepared folk mlp baseline

--- cluster/run_baselines/run-baselines.sh
+++ cluster/run_baselines/run-baselines.sh
@@ -1,23 +1,8 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "diabetes dt_clf 1,2,3,4,5,6"
-    "diabetes lr_clf 1,2,3,4,5,6"
-    "diabetes lgbm_clf 1,2,3,4,5,6"
-    "diabetes rf_clf 1,2,3,4,5,6"
-    "diabetes mlp_clf 1,2,3"
-    "diabetes mlp_clf 4,5,6"
-    "folk dt_clf 1,2,3,4,5,6"
-    "folk lr_clf 1,2,3,4,5,6"
-    "folk lgbm_clf 1,2,3,4,5,6"
-    "folk rf_clf 1,2,3,4,5,6"
-    "folk mlp_clf 1,2,3"
-    "folk mlp_clf 4,5,6"
-    "law_school dt_clf 1,2,3,4,5,6"
-    "law_school lr_clf 1,2,3,4,5,6"
-    "law_school lgbm_clf 1,2,3,4,5,6"
-    "law_school rf_clf 1,2,3,4,5,6"
-    "law_school mlp_clf 1,2,3"
-    "law_school mlp_clf 4,5,6"
+    "folk mlp_clf 1,2"
+    "folk mlp_clf 3,4"
+    "folk mlp_clf 5,6"
 )
 
 TEMPLATE_FILE="../cluster/run_baselines/run-baselines-template.sbatch"
