37d7ebe8fed2da46e0fa1c6c9c1ce1fae533c8f6
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-05-03 15:39:13 +0300

    Checked disparity box plots for model performance

--- source/visualizations/models_viz.py
+++ source/visualizations/models_viz.py
@@ -1,9 +1,36 @@
+import pandas as pd
 import altair as alt
 import seaborn as sns
 from altair.utils.schemapi import Undefined
+from virny.custom_classes.metrics_composer import MetricsComposer
+from virny.configs.constants import *
 
 from configs.constants import EXP_COLLECTION_NAME
 from configs.scenarios_config import EVALUATION_SCENARIOS_CONFIG
+
+
+def get_overall_metric_from_disparity_metric(disparity_metric):
+    overall_to_disparity_metric_dct = {
+        # Error disparity metrics
+        TPR: [EQUALIZED_ODDS_TPR],
+        TNR: [EQUALIZED_ODDS_TNR],
+        FPR: [EQUALIZED_ODDS_FPR],
+        FNR: [EQUALIZED_ODDS_FNR],
+        ACCURACY: [ACCURACY_DIFFERENCE],
+        SELECTION_RATE: [STATISTICAL_PARITY_DIFFERENCE, DISPARATE_IMPACT],
+        # Stability disparity metrics
+        LABEL_STABILITY: [LABEL_STABILITY_RATIO, LABEL_STABILITY_DIFFERENCE],
+        JITTER: [JITTER_DIFFERENCE],
+        IQR: [IQR_DIFFERENCE],
+        # Uncertainty disparity metrics
+        STD: [STD_DIFFERENCE, STD_RATIO],
+        OVERALL_UNCERTAINTY: [OVERALL_UNCERTAINTY_DIFFERENCE, OVERALL_UNCERTAINTY_RATIO],
+        ALEATORIC_UNCERTAINTY: [ALEATORIC_UNCERTAINTY_DIFFERENCE, ALEATORIC_UNCERTAINTY_RATIO],
+        EPISTEMIC_UNCERTAINTY: [EPISTEMIC_UNCERTAINTY_DIFFERENCE, EPISTEMIC_UNCERTAINTY_RATIO],
+    }
+    for overall_metric in overall_to_disparity_metric_dct.keys():
+        if disparity_metric in overall_to_disparity_metric_dct[overall_metric]:
+            return overall_metric
 
 
 def get_models_metric_df(db_client, dataset_name: str, evaluation_scenario: str,
@@ -27,6 +54,54 @@ def get_models_metric_df(db_client, dataset_name: str, evaluation_scenario: str,
     metric_df = metric_df[columns_subset]
 
     return metric_df
+
+
+def get_models_disparity_metric_df(db_client, dataset_name: str, evaluation_scenario: str, metric_name: str, group: str):
+    dis_grp_models_metric_df = get_models_metric_df(db_client=db_client,
+                                                    dataset_name=dataset_name,
+                                                    evaluation_scenario=evaluation_scenario,
+                                                    metric_name=metric_name,
+                                                    group=group + '_dis')
+    priv_grp_models_metric_df = get_models_metric_df(db_client=db_client,
+                                                     dataset_name=dataset_name,
+                                                     evaluation_scenario=evaluation_scenario,
+                                                     metric_name=metric_name,
+                                                     group=group + '_priv')
+    grp_models_metric_df = pd.concat([dis_grp_models_metric_df, priv_grp_models_metric_df])
+
+    # Compose group metrics
+    disparity_metric_df = pd.DataFrame()
+    for null_imputer_name in grp_models_metric_df['Null_Imputer_Name'].unique():
+        for exp_seed in grp_models_metric_df['Virny_Random_State'].unique():
+            for model_name in grp_models_metric_df['Model_Name'].unique():
+                for test_set_index in grp_models_metric_df['Test_Set_Index'].unique():
+                    model_subgroup_metrics_df = grp_models_metric_df[
+                        (grp_models_metric_df['Null_Imputer_Name'] == null_imputer_name) &
+                        (grp_models_metric_df['Virny_Random_State'] == exp_seed) &
+                        (grp_models_metric_df['Model_Name'] == model_name) &
+                        (grp_models_metric_df['Test_Set_Index'] == test_set_index)
+                    ]
+
+                    # Create columns based on values in the Subgroup column
+                    pivoted_model_subgroup_metrics_df = model_subgroup_metrics_df.pivot(columns='Subgroup', values='Metric_Value',
+                                                                                        index=[col for col in model_subgroup_metrics_df.columns
+                                                                                               if col not in ('Subgroup', 'Metric_Value')]).reset_index()
+                    pivoted_model_subgroup_metrics_df = pivoted_model_subgroup_metrics_df.rename_axis(None, axis=1)
+
+                    metrics_composer = MetricsComposer(
+                        {model_name: pivoted_model_subgroup_metrics_df},
+                        sensitive_attributes_dct={group: None}
+                    )
+                    model_group_metrics_df = metrics_composer.compose_metrics()
+                    model_group_metrics_df['Null_Imputer_Name'] = null_imputer_name
+                    model_group_metrics_df['Evaluation_Scenario'] = evaluation_scenario
+                    model_group_metrics_df['Virny_Random_State'] = exp_seed
+                    model_group_metrics_df['Model_Name'] = model_name
+                    model_group_metrics_df['Test_Set_Index'] = test_set_index
+
+                    disparity_metric_df = pd.concat([disparity_metric_df, model_group_metrics_df])
+
+    return disparity_metric_df
 
 
 def create_scatter_plots_for_diff_models_and_single_eval_scenario(dataset_name: str, evaluation_scenario: str,
@@ -282,12 +357,23 @@ def create_box_plots_for_diff_imputers_and_single_eval_scenario_v2(dataset_name:
     imputers_order = ['deletion', 'median-mode', 'median-dummy', 'miss_forest',
                       'k_means_clustering', 'datawig', 'automl']
 
-    metric_name = '_'.join([c.capitalize() for c in metric_name.split('_')])
-    models_metric_df = get_models_metric_df(db_client=db_client,
-                                            dataset_name=dataset_name,
-                                            evaluation_scenario=evaluation_scenario,
-                                            metric_name=metric_name,
-                                            group=group)
+    metric_name = '_'.join([c.capitalize() for c in metric_name.split('_')]) if 'equalized_odds' not in metric_name.lower() else metric_name
+    if group == 'overall':
+        models_metric_df = get_models_metric_df(db_client=db_client,
+                                                dataset_name=dataset_name,
+                                                evaluation_scenario=evaluation_scenario,
+                                                metric_name=metric_name,
+                                                group=group)
+    else:
+        overall_metric = get_overall_metric_from_disparity_metric(disparity_metric=metric_name)
+        models_metric_df = get_models_disparity_metric_df(db_client=db_client,
+                                                          dataset_name=dataset_name,
+                                                          evaluation_scenario=evaluation_scenario,
+                                                          metric_name=overall_metric,
+                                                          group=group)
+        models_metric_df = models_metric_df[models_metric_df['Metric'] == metric_name]
+        models_metric_df = models_metric_df.rename(columns={group: 'Metric_Value'})
+
     models_metric_df = models_metric_df[models_metric_df['Model_Name'] == model_name]
     models_metric_df['Test_Injection_Strategy'] = models_metric_df.apply(
         lambda row: EVALUATION_SCENARIOS_CONFIG[row['Evaluation_Scenario']]['test_injection_scenarios'][row['Test_Set_Index']][:-1],
@@ -332,6 +418,7 @@ def create_box_plots_for_diff_imputers_v2(dataset_name: str, model_name: str, me
                                                                                  group=group,
                                                                                  base_font_size=base_font_size,
                                                                                  ylim=ylim)
+    print('Prepared a plot for an MCAR train set')
     base_chart2 = create_box_plots_for_diff_imputers_and_single_eval_scenario_v2(dataset_name=dataset_name,
                                                                                  evaluation_scenario='exp1_mar3',
                                                                                  title='MAR train set',
@@ -341,6 +428,7 @@ def create_box_plots_for_diff_imputers_v2(dataset_name: str, model_name: str, me
                                                                                  group=group,
                                                                                  base_font_size=base_font_size,
                                                                                  ylim=ylim)
+    print('Prepared a plot for an MAR train set')
     base_chart3 = create_box_plots_for_diff_imputers_and_single_eval_scenario_v2(dataset_name=dataset_name,
                                                                                  evaluation_scenario='exp1_mnar3',
                                                                                  title='MNAR train set',
@@ -350,6 +438,7 @@ def create_box_plots_for_diff_imputers_v2(dataset_name: str, model_name: str, me
                                                                                  group=group,
                                                                                  base_font_size=base_font_size,
                                                                                  ylim=ylim)
+    print('Prepared a plot for an MNAR train set')
 
     # Concatenate two base charts
     main_base_chart = alt.vconcat()
