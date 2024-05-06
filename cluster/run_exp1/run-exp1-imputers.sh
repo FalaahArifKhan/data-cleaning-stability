7cf2b27635f3274a180eba751ff016a9a15fafdb
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-05-06 17:47:28 +0300

    Prepared a bash script to start diabetes cpu jobs

--- cluster/run_exp1/run-exp1-imputers.sh
+++ cluster/run_exp1/run-exp1-imputers.sh
@@ -1,14 +1,14 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "german automl exp1_mcar3 2"
-    "german automl exp1_mcar3 4"
-    "german automl exp1_mcar3 6"
-    "german automl exp1_mar3 2"
-    "german automl exp1_mar3 4"
-    "german automl exp1_mar3 6"
-    "german automl exp1_mnar3 2"
-    "german automl exp1_mnar3 4"
-    "german automl exp1_mnar3 6"
+    "diabetes automl exp1_mcar3 1,2"
+    "diabetes automl exp1_mcar3 3,4"
+    "diabetes automl exp1_mcar3 5,6"
+    "diabetes automl exp1_mar3 1,2"
+    "diabetes automl exp1_mar3 3,4"
+    "diabetes automl exp1_mar3 5,6"
+    "diabetes automl exp1_mnar3 1,2"
+    "diabetes automl exp1_mnar3 3,4"
+    "diabetes automl exp1_mnar3 5,6"
 )
 
 TEMPLATE_FILE="../cluster/run_exp1/run-exp1-imputers-template.sbatch"
