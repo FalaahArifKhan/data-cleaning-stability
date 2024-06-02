4ca591f3ee4231e101ca9fa68c49d064e8e1373c
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-06-03 00:02:23 +0300

    Prepared bash scripts to run models

---
+++ cluster/run_mixed_exp/run-mixed-exp-imputers-gpu.sh
@@ -0,0 +1,50 @@
+# Define the list of tuples (dataset_name, model_name, run_nums)
+declare -a job_configs=(
+    "bank datawig exp1_mcar3 1"
+    "bank datawig exp1_mcar3 2"
+    "bank datawig exp1_mcar3 3"
+    "bank datawig exp1_mcar3 4"
+    "bank datawig exp1_mcar3 5"
+    "bank datawig exp1_mcar3 6"
+    "bank datawig exp1_mar3 1"
+    "bank datawig exp1_mar3 2"
+    "bank datawig exp1_mar3 3"
+    "bank datawig exp1_mar3 4"
+    "bank datawig exp1_mar3 5"
+    "bank datawig exp1_mar3 6"
+    "bank datawig exp1_mnar3 1"
+    "bank datawig exp1_mnar3 2"
+    "bank datawig exp1_mnar3 3"
+    "bank datawig exp1_mnar3 4"
+    "bank datawig exp1_mnar3 5"
+    "bank datawig exp1_mnar3 6"
+)
+
+TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-imputers-gpu-template.sbatch"
+
+# Initialize a counter
+index=0
+
+# Iterate through the array of job_configs
+for job_config in "${job_configs[@]}"
+do
+    # Split the job_config into separate variables
+    read -r dataset null_imputer evaluation_scenario run_nums <<< "$job_config"
+
+    # Define the output file name
+    output_file="../cluster/run_mixed_exp/sbatch_files/imputers/run-mixed-exp-imputers-${dataset}_${null_imputer}_${evaluation_scenario}_${index}.sbatch"
+
+    # Create an empty file
+    touch $output_file
+
+    # Use sed to replace placeholders with actual values
+    sed -e "s/<DATASET>/${dataset}/g" -e "s/<EVALUATION_SCENARIO>/${evaluation_scenario}/g" -e "s/<NULL_IMPUTER>/${null_imputer}/g" -e "s/<RUN_NUMS>/${run_nums}/g" $TEMPLATE_FILE > $output_file
+
+    # Execute a SLURM job
+    sbatch $output_file
+
+    echo "Job was executed: $output_file"
+
+    # Increment the index
+    ((index++))
+done
---
+++ cluster/run_mixed_exp/run-mixed-exp-imputers.sh
@@ -0,0 +1,36 @@
+# Define the list of tuples (dataset_name, model_name, run_nums)
+declare -a job_configs=(
+    "bank automl exp1_mcar3 1"
+    "bank automl exp1_mcar3 2"
+    "bank automl exp1_mcar3 3"
+    "bank automl exp1_mcar3 4"
+)
+
+TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-imputers-template.sbatch"
+
+# Initialize a counter
+index=0
+
+# Iterate through the array of job_configs
+for job_config in "${job_configs[@]}"
+do
+    # Split the job_config into separate variables
+    read -r dataset null_imputer evaluation_scenario run_nums <<< "$job_config"
+
+    # Define the output file name
+    output_file="../cluster/run_mixed_exp/sbatch_files/imputers/run-mixed-exp-imputers-${dataset}_${null_imputer}_${evaluation_scenario}_${index}.sbatch"
+
+    # Create an empty file
+    touch $output_file
+
+    # Use sed to replace placeholders with actual values
+    sed -e "s/<DATASET>/${dataset}/g" -e "s/<EVALUATION_SCENARIO>/${evaluation_scenario}/g" -e "s/<NULL_IMPUTER>/${null_imputer}/g" -e "s/<RUN_NUMS>/${run_nums}/g" $TEMPLATE_FILE > $output_file
+
+    # Execute a SLURM job
+    sbatch $output_file
+
+    echo "Job was executed: $output_file"
+
+    # Increment the index
+    ((index++))
+done
