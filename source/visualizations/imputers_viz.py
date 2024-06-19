178eef22527592aceb7497ef8527cdd97ef16be6
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-06-19 15:04:00 +0300

    Fixed legend offset

--- source/visualizations/imputers_viz.py
+++ source/visualizations/imputers_viz.py
@@ -197,7 +197,7 @@ def create_box_plots_for_diff_imputers(dataset_name: str, column_name: str,
             orient='top',
             direction='horizontal',
             titleAnchor='middle',
-            symbolOffset=156,
+            symbolOffset=150 if without_dummy else 120,
         ).configure_facet(
             spacing=10
         ).configure_view(
