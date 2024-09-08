70ffd23622d29c951c8023a619d591d159965e66
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-09 01:51:29 +0300

    Executed pipelines for diabetes and german with tabpfn

--- configs/virny_wrappers.py
+++ configs/virny_wrappers.py
@@ -3,9 +3,6 @@ from tabpfn import TabPFNClassifier
 
 class TabPFNClassifierWrapper(TabPFNClassifier):
     def set_params(self, **params):
-        import warnings
-        warnings.filterwarnings('ignore')
-
         if 'random_state' in params.keys():
             params['seed'] = params.pop('random_state')
 
