8b91d513964fa9ba5912b84483b3f25d76c15bf4
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-18 02:37:32 +0300

    Prepared baseline jobs for folk

--- cluster/run_baselines/run-baselines.sh
+++ cluster/run_baselines/run-baselines.sh
@@ -1,5 +1,7 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
+    "bank gandalf_clf 6"
+    "diabetes gandalf_clf 1,2,3,4,5,6"
     "folk gandalf_clf 1,2"
     "folk gandalf_clf 3,4"
     "folk gandalf_clf 5,6"
