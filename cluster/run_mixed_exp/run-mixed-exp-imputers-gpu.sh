f3b6ed3bdde8fcda4944aa2544019b38fbd109a6
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-23 22:13:53 +0300

    Prepared jobs for folk and mixed_exp

--- cluster/run_mixed_exp/run-mixed-exp-imputers-gpu.sh
+++ cluster/run_mixed_exp/run-mixed-exp-imputers-gpu.sh
@@ -1,8 +1,8 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "heart datawig mixed_exp 1,2"
-    "heart datawig mixed_exp 3,4"
-    "heart datawig mixed_exp 5,6"
+    "folk datawig mixed_exp 1,2"
+    "folk datawig mixed_exp 3,4"
+    "folk datawig mixed_exp 5,6"
 )
 
 TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-imputers-gpu-template.sbatch"
