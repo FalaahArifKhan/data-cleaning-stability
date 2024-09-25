69d04c7b4c31dd4f53575f404767139b858ad0e1
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-26 01:25:22 +0300

    Prepared jobs for folk_emp

--- scripts/test.py
+++
@@ -1,4 +0,0 @@
-from virny.datasets import ACSEmploymentDataset
-
-data_loader = ACSEmploymentDataset(state=['CA'], year=2018, with_nulls=False)
-print('data_loader.full_df.shape:', data_loader.full_df.shape)
