a3fd7c051da2f35adf9ccd1706295e2300e6ac5d
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-28 00:43:17 +0300

    Prepared jobs for miss_forst and datawig for folk_emp

--- cluster/run_mixed_exp/run-mixed-exp-models.sh
+++ cluster/run_mixed_exp/run-mixed-exp-models.sh
@@ -1,39 +1,7 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "folk_emp deletion mixed_exp gandalf_clf 1"
-    "folk_emp deletion mixed_exp gandalf_clf 2"
-    "folk_emp deletion mixed_exp gandalf_clf 3"
-    "folk_emp deletion mixed_exp gandalf_clf 4"
-    "folk_emp deletion mixed_exp gandalf_clf 5"
-    "folk_emp deletion mixed_exp gandalf_clf 6"
-
-    "folk_emp median-mode mixed_exp gandalf_clf 1"
-    "folk_emp median-mode mixed_exp gandalf_clf 2"
-    "folk_emp median-mode mixed_exp gandalf_clf 3"
-    "folk_emp median-mode mixed_exp gandalf_clf 4"
-    "folk_emp median-mode mixed_exp gandalf_clf 5"
-    "folk_emp median-mode mixed_exp gandalf_clf 6"
-
-    "folk_emp median-dummy mixed_exp gandalf_clf 1"
-    "folk_emp median-dummy mixed_exp gandalf_clf 2"
-    "folk_emp median-dummy mixed_exp gandalf_clf 3"
-    "folk_emp median-dummy mixed_exp gandalf_clf 4"
-    "folk_emp median-dummy mixed_exp gandalf_clf 5"
-    "folk_emp median-dummy mixed_exp gandalf_clf 6"
-
     "folk_emp miss_forest mixed_exp gandalf_clf 1"
-    "folk_emp miss_forest mixed_exp gandalf_clf 2"
-    "folk_emp miss_forest mixed_exp gandalf_clf 3"
-    "folk_emp miss_forest mixed_exp gandalf_clf 4"
     "folk_emp miss_forest mixed_exp gandalf_clf 5"
-    "folk_emp miss_forest mixed_exp gandalf_clf 6"
-
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 1"
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 2"
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 3"
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 4"
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 5"
-    "folk_emp k_means_clustering mixed_exp gandalf_clf 6"
 )
 
 TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-models-template.sbatch"
