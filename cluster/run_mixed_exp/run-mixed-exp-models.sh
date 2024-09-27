ed9bfc91b09432aebbdd3430c440977281ba7452
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-28 01:17:47 +0300

    Prepared jobs for miss_forst and datawig for folk_emp

--- cluster/run_mixed_exp/run-mixed-exp-models.sh
+++ cluster/run_mixed_exp/run-mixed-exp-models.sh
@@ -1,7 +1,11 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "folk_emp miss_forest mixed_exp gandalf_clf 1"
-    "folk_emp miss_forest mixed_exp gandalf_clf 5"
+    "folk_emp datawig mixed_exp gandalf_clf 1"
+    "folk_emp datawig mixed_exp gandalf_clf 2"
+    "folk_emp datawig mixed_exp gandalf_clf 3"
+    "folk_emp datawig mixed_exp gandalf_clf 4"
+    "folk_emp datawig mixed_exp gandalf_clf 5"
+    "folk_emp datawig mixed_exp gandalf_clf 6"
 )
 
 TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-models-template.sbatch"
