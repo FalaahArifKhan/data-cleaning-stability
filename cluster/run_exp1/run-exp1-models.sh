c7123d3df9841a9087f6f0f126ff3898de9ca5b5
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-29 11:15:33 +0300

    Prepared boost_clean jobs

--- cluster/run_exp1/run-exp1-models.sh
+++ cluster/run_exp1/run-exp1-models.sh
@@ -1,16 +1,22 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "folk_emp boost_clean exp1_mcar3 gandalf_clf 1"
-    "folk_emp boost_clean exp1_mcar3 gandalf_clf 3"
-    "folk_emp boost_clean exp1_mcar3 gandalf_clf 5"
+    "folk_emp automl exp1_mcar3 gandalf_clf 2"
+    "folk_emp automl exp1_mcar3 gandalf_clf 4"
+    "folk_emp automl exp1_mcar3 gandalf_clf 6"
+    "folk_emp automl exp1_mar3 gandalf_clf 2"
+    "folk_emp automl exp1_mar3 gandalf_clf 4"
+    "folk_emp automl exp1_mar3 gandalf_clf 6"
+    "folk_emp automl exp1_mnar3 gandalf_clf 4"
+    "folk_emp automl exp1_mnar3 gandalf_clf 6"
 
-    "folk_emp boost_clean exp1_mar3 gandalf_clf 1"
-    "folk_emp boost_clean exp1_mar3 gandalf_clf 3"
-    "folk_emp boost_clean exp1_mar3 gandalf_clf 5"
-
-    "folk_emp boost_clean exp1_mnar3 gandalf_clf 1"
-    "folk_emp boost_clean exp1_mnar3 gandalf_clf 3"
-    "folk_emp boost_clean exp1_mnar3 gandalf_clf 5"
+    "folk_emp boost_clean exp1_mcar3 gandalf_clf 2"
+    "folk_emp boost_clean exp1_mcar3 gandalf_clf 4"
+    "folk_emp boost_clean exp1_mcar3 gandalf_clf 6"
+    "folk_emp boost_clean exp1_mar3 gandalf_clf 2"
+    "folk_emp boost_clean exp1_mar3 gandalf_clf 4"
+    "folk_emp boost_clean exp1_mar3 gandalf_clf 6"
+    "folk_emp boost_clean exp1_mnar3 gandalf_clf 4"
+    "folk_emp boost_clean exp1_mnar3 gandalf_clf 6"
 )
 
 TEMPLATE_FILE="../cluster/run_exp1/run-exp1-models-template.sbatch"
