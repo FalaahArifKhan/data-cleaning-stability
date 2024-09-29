737661a018b58f26d7c469b6934782fdea4a074a
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-09-30 01:48:55 +0300

    Prepared automl jobs

--- cluster/run_mixed_exp/run-mixed-exp-models.sh
+++ cluster/run_mixed_exp/run-mixed-exp-models.sh
@@ -1,8 +1,10 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
-    "folk_emp boost_clean mixed_exp gandalf_clf 1"
-    "folk_emp boost_clean mixed_exp gandalf_clf 3"
-    "folk_emp boost_clean mixed_exp gandalf_clf 5"
+    "folk_emp automl mixed_exp gandalf_clf 2"
+    "folk_emp automl mixed_exp gandalf_clf 4"
+
+#     "folk_emp boost_clean mixed_exp gandalf_clf 2"
+#     "folk_emp boost_clean mixed_exp gandalf_clf 4"
 )
 
 TEMPLATE_FILE="../cluster/run_mixed_exp/run-mixed-exp-models-template.sbatch"
