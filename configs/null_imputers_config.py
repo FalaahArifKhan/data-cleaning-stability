eec0ea8bedf43b969c5eaead9a9c07bb49cbdd03
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-25 22:59:08 +0200

    Rechecked default args for new MVI techniques

--- configs/null_imputers_config.py
+++ configs/null_imputers_config.py
@@ -1,8 +1,7 @@
 import source.null_imputers.datawig_imputer as datawig_imputer
 
 from configs.constants import (GERMAN_CREDIT_DATASET, BANK_MARKETING_DATASET, CARDIOVASCULAR_DISEASE_DATASET,
-                               DIABETES_DATASET, LAW_SCHOOL_DATASET, ACS_INCOME_DATASET,
-                               ErrorRepairMethod, ErrorInjectionStrategy)
+                               DIABETES_DATASET, LAW_SCHOOL_DATASET, ACS_INCOME_DATASET, ErrorRepairMethod)
 from source.null_imputers.imputation_methods import (impute_with_deletion, impute_with_simple_imputer,
                                                      impute_with_automl,
                                                      impute_with_gain, impute_with_missforest, impute_with_kmeans,
