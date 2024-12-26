218247869186acef5dfb615a1292dea086c603b3
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-27 01:15:49 +0200

    Prepared run scripts for GAIN

--- cluster/run_exp1/run-exp1-imputers-gain.sh
+++ cluster/run_exp1/run-exp1-imputers-gain.sh
@@ -1,12 +1,48 @@
 # Define the list of tuples (dataset_name, model_name, run_nums)
 declare -a job_configs=(
 #    "diabetes gain exp1_mcar3 1,2,3"
+    "diabetes gain exp1_mcar3 4,5,6"
+    "diabetes gain exp1_mar3 1,2,3,4,5,6"
+    "diabetes gain exp1_mnar3 1,2,3,4,5,6"
+    "diabetes gain mixed_exp 1,2,3,4,5,6"
+
 #    "german gain exp1_mcar3 1,2,3"
-#    "law_school gain exp1_mcar3 1,2"
-#    "folk gain exp1_mcar3 1"
-#    "bank gain exp1_mcar3 1"
-#    "heart gain exp1_mcar3 1"
-    "folk_emp gain exp1_mcar3 1"
+    "german gain exp1_mcar3 4,5,6"
+    "german gain exp1_mar3 1,2,3,4,5,6"
+    "german gain exp1_mnar3 1,2,3,4,5,6"
+    "german gain mixed_exp 1,2,3,4,5,6"
+
+##    "law_school gain exp1_mcar3 1,2"
+#    "law_school gain exp1_mcar3 3,4,5,6"
+#    "law_school gain exp1_mar3 1,2,3,4,5,6"
+#    "law_school gain exp1_mnar3 1,2,3,4,5,6"
+#    "law_school gain mixed_exp 1,2,3,4,5,6"
+#
+##    "folk gain exp1_mcar3 1"
+#    "folk gain exp1_mcar3 2,3,4,5,6"
+#    "folk gain exp1_mar3 1,2,3,4,5,6"
+#    "folk gain exp1_mnar3 1,2,3,4,5,6"
+#    "folk gain mixed_exp 1,2,3,4,5,6"
+#
+##    "bank gain exp1_mcar3 1"
+#    "bank gain exp1_mcar3 2,3,4,5,6"
+#    "bank gain exp1_mar3 1,2,3,4,5,6"
+#    "bank gain exp1_mnar3 1,2,3,4,5,6"
+#    "bank gain mixed_exp 1,2,3,4,5,6"
+#
+##    "heart gain exp1_mcar3 1"
+#    "heart gain exp1_mcar3 2,3,4,5,6"
+#    "heart gain exp1_mar3 1,2,3,4,5,6"
+#    "heart gain exp1_mnar3 1,2,3,4,5,6"
+#    "heart gain mixed_exp 1,2,3,4,5,6"
+#
+#
+#
+##    "folk_emp gain exp1_mcar3 1"
+#    "folk_emp gain exp1_mcar3 2,3,4,5,6"
+#    "folk_emp gain exp1_mar3 1,2,3,4,5,6"
+#    "folk_emp gain exp1_mnar3 1,2,3,4,5,6"
+#    "folk_emp gain mixed_exp 1,2,3,4,5,6"
 )
 
 TEMPLATE_FILE="../cluster/run_exp1/run-exp1-imputers-template.sbatch"
