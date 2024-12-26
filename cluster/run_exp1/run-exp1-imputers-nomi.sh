df3a331fbff24d8e7b25fe8012fa331c06671f06
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-26 19:28:23 +0200

    Prepared run scripts for NOMI

--- cluster/run_exp1/run-exp1-imputers-nomi.sh
+++ cluster/run_exp1/run-exp1-imputers-nomi.sh
@@ -5,8 +5,8 @@ declare -a job_configs=(
 #    "law_school nomi exp1_mcar3 1,2"
 #    "folk nomi exp1_mcar3 1,2"
 #    "bank nomi exp1_mcar3 1"
-    "heart nomi exp1_mcar3 1"
-#    "folk_emp nomi exp1_mcar3 1"
+#    "heart nomi exp1_mcar3 1"
+    "folk_emp nomi exp1_mcar3 1"
 )
 
 TEMPLATE_FILE="../cluster/run_exp1/run-exp1-imputers-template-nomi.sbatch"
