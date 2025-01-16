dc857b9ab9c5dc4ead59ebcb294120671a6727d5
Denys Herasymuk <herasymuk@ucu.edu.ua>
2025-01-16 02:42:30 +0200

    wip

--- cluster/run_exp1/run-exp1-imputers.sh
+++ cluster/run_exp1/run-exp1-imputers.sh
@@ -19,21 +19,29 @@ declare -a job_configs=(
 #    "folk hivae exp1_mar3 1,2,3,4,5,6"
 #    "folk hivae exp1_mnar3 1,2,3,4,5,6"
 #    "folk hivae mixed_exp 1,2,3,4,5,6"
-#
-#    "bank hivae exp1_mcar3 1,2,3,4,5,6"
-#    "bank hivae exp1_mar3 1,2,3,4,5,6"
-#    "bank hivae exp1_mnar3 1,2,3,4,5,6"
-#    "bank hivae mixed_exp 1,2,3,4,5,6"
-#
-#    "heart hivae exp1_mcar3 1,2,3,4,5,6"
-#    "heart hivae exp1_mar3 1,2,3,4,5,6"
-#    "heart hivae exp1_mnar3 1,2,3,4,5,6"
-#    "heart hivae mixed_exp 1,2,3,4,5,6"
-#
-    "folk_emp hivae exp1_mcar3 1,2,3,4,5,6"
-    "folk_emp hivae exp1_mar3 1,2,3,4,5,6"
-    "folk_emp hivae exp1_mnar3 1,2,3,4,5,6"
-    "folk_emp hivae mixed_exp 1,2,3,4,5,6"
+
+    "bank hivae exp1_mcar3 1,2,3"
+    "bank hivae exp1_mcar3 4,5,6"
+    "bank hivae exp1_mar3 1,2,3"
+    "bank hivae exp1_mar3 4,5,6"
+    "bank hivae exp1_mnar3 1,2,3"
+    "bank hivae exp1_mnar3 4,5,6"
+    "bank hivae mixed_exp 1,2,3"
+    "bank hivae mixed_exp 4,5,6"
+
+    "heart hivae exp1_mcar3 1,2,3"
+    "heart hivae exp1_mcar3 4,5,6"
+    "heart hivae exp1_mar3 1,2,3"
+    "heart hivae exp1_mar3 4,5,6"
+    "heart hivae exp1_mnar3 1,2,3"
+    "heart hivae exp1_mnar3 4,5,6"
+    "heart hivae mixed_exp 1,2,3"
+    "heart hivae mixed_exp 4,5,6"
+
+#    "folk_emp hivae exp1_mcar3 1,2,3,4,5,6"
+#    "folk_emp hivae exp1_mar3 1,2,3,4,5,6"
+#    "folk_emp hivae exp1_mnar3 1,2,3,4,5,6"
+#    "folk_emp hivae mixed_exp 1,2,3,4,5,6"
 )
 
 TEMPLATE_FILE="../cluster/run_exp1/run-exp1-imputers-template.sbatch"
